24cd733771443fca40748f2b04d162f7b5c70dc3
ok <roulette@ualberta.ca>
2021-10-07 01:10:16 -0400

    license

--- httpclient.py
+++ httpclient.py
@@ -13,6 +13,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# Modifications Copyright (C) 2021 Keaton Roulette
 
 # Do not use urllib's HTTP GET and POST mechanisms.
 # Write your own HTTP GET and POST
